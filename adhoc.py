5c7e35b8def1d874480bf19da4e8d3178d0c8500
Richard Guan <rguan72@gmail.com>
2024-07-21 19:01:16 -0700

    .

--- adhoc.py
+++ adhoc.py
@@ -20,12 +20,13 @@ load_dotenv()
 # smart_plugs.turn_all_lights_off()
 
 # speakers.list_audio_devices()
-# speakers.set_device_by_name("JBL")
+speakers.set_device_by_name("JBL")
 smart_plugs.turn_all_lights_off()
 smart_plugs.turn_all_lights_on()
 speakers.play_clue_solved_sound_effect()
 time.sleep(1)
 smart_plugs.turn_all_lights_off()
 
-# print(smart_plugs.get_lights())
 # smart_plugs.turn_all_lights_off()
+
+# smart_plugs.light_effect()
--- lights/smart_plugs.py
+++ lights/smart_plugs.py
@@ -1,14 +1,15 @@
 import requests
 import common.constants as constants
+import time
 
 def get_lights():
     response = requests.get(f'http://{constants.bridge_ip}/api/{constants.username}/lights')
     return response.json()
 
-def control_light(light_id, state):
+def control_light(light_id, state, transitiontime=0):
     payload = {
         "on": state,
-        "transitiontime": 0,
+        "transitiontime": transitiontime,
     }
     response = requests.put(f'http://{constants.bridge_ip}/api/{constants.username}/lights/{light_id}/state', json=payload)
     return response.json()
@@ -23,3 +24,27 @@ def turn_all_lights_on():
 
 def turn_all_lights_off():
     control_all_lights(False)
+
+def light_effect():
+    lights = get_lights()
+    for id, body in lights.items():
+        is_lightbulb = "Hue color" in body["productname"]
+        if is_lightbulb:
+            set_lightbulb_color(id, 60, 100, 100)
+            time.sleep(1)
+            control_light(id, False)
+            time.sleep(1)
+            control_light(id, True)
+            time.sleep(1)
+            control_light(id, False)
+
+def set_lightbulb_color(light_id, hue, sat, bri, transitiontime=0):
+    payload = {
+        "on": True,
+        'hue': hue,
+        'sat': sat,
+        'bri': bri,
+        'transitiontime': transitiontime,
+    }
+    response = requests.put(f"http://{constants.bridge_ip}/api/{constants.username}/lights/{light_id}/state", json=payload)
+    return response.json()
