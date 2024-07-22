efa0dfc23c40c0d7b21ad62ca6247369e03b63c0
Richard Guan <rguan72@gmail.com>
2024-07-21 17:35:03 -0700

    .

--- adhoc.py
+++ adhoc.py
@@ -1,12 +1,28 @@
 from lights import smart_plugs as smart_plugs
 from sounds import speakers as speakers
 from dotenv import load_dotenv
+import time
 import os
 
 load_dotenv()
 
 # print(smart_plugs.get_lights())
-# smart_plugs.turn_all_lights_off()
+# # smart_plugs.turn_all_lights_off()
 # print(smart_plugs.get_lights())
+# # smart_plugs.turn_all_lights_on()
 
-speakers.list_audio_devices()
+# smart_plugs.turn_all_lights_on()
+# time.sleep(1)
+# smart_plugs.turn_all_lights_off()
+# time.sleep(1)
+# smart_plugs.turn_all_lights_on()
+# time.sleep(1)
+# smart_plugs.turn_all_lights_off()
+
+# speakers.list_audio_devices()
+# speakers.set_device_by_name("JBL")
+smart_plugs.turn_all_lights_off()
+smart_plugs.turn_all_lights_on()
+speakers.play_clue_solved_sound_effect()
+time.sleep(1)
+smart_plugs.turn_all_lights_off()
--- sounds/speakers.py
+++ sounds/speakers.py
@@ -1,6 +1,31 @@
 import sounddevice as sd
+import numpy as np
+from pydub import AudioSegment
 
 def list_audio_devices():
     devices = sd.query_devices()
     for idx, device in enumerate(devices):
         print(f"{idx}: {device['name']}")
+        print(device)
+
+def set_device_by_name(device_name):
+    devices = sd.query_devices()
+    for _, device in enumerate(devices):
+        if device_name in device['name']:
+            sd.default.device = device['name']
+            print(f"Device set to: {device['name']}")
+            return
+
+def play_mp3(file_path):
+    set_device_by_name("JBL")
+    # Load the MP3 file
+    audio = AudioSegment.from_file(file_path, format="mp3")
+    # Convert to numpy array
+    audio_data = np.array(audio.get_array_of_samples())
+    # Play the audio data
+    sd.default.device = "JBL Charge 3"
+    sd.play(audio_data, audio.frame_rate)
+    sd.wait()  # Wait until the file has finished playing
+
+def play_clue_solved_sound_effect():
+    play_mp3("sounds/assets/clue_solved.mp3")
