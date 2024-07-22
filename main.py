5a724541e8ead194464879292a6c1069240a899a
Richard Guan <rguan72@gmail.com>
2024-07-21 17:46:30 -0700

    .

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from lights import smart_plugs as smart_plugs
+from sounds import speakers as speakers
+import time
 
 from common import constants
 
@@ -14,3 +16,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/trigger_effects")
+async def trigger():
+    smart_plugs.turn_all_lights_off()
+    smart_plugs.turn_all_lights_on()
+    speakers.play_clue_solved_sound_effect()
+    time.sleep(1)
+    smart_plugs.turn_all_lights_off()
